36c1e6a0748db732b6f34eacdef942547b0fae78
gcdpinho <gcdpinho@gmail.com>
2018-07-20 20:58:59 -0300

    news + videos

--- src/app/controllers/newsController.js
+++ src/app/controllers/newsController.js
@@ -15,7 +15,6 @@ router.get('/byApproval', (req, res) => {
                 res.json(news);
             })
             .catch(err => {
-                console.log(err);
                 res.status(400).send({
                     err: err,
                     position: 0,
@@ -33,7 +32,7 @@ router.get('/byApproval', (req, res) => {
 
 router.post('/byTag', (req, res) => {
     try {
-        News.forge({
+        News.where({
                 approval: 1,
                 flgActive: 1
             })
@@ -47,7 +46,6 @@ router.post('/byTag', (req, res) => {
                 res.json(news);
             })
             .catch(err => {
-                console.log(err);
                 res.status(400).send({
                     err: err,
                     position: 0,
@@ -55,11 +53,106 @@ router.post('/byTag', (req, res) => {
                 });
             });
     } catch (err) {
-        console.log(err);
         res.status(400).send({
             err: err,
             position: 1,
             message: 'Error listing news by tag.'
+        });
+    }
+});
+
+router.post('/byCategory', (req, res) => {
+    try {
+        News.where({
+                approval: 1,
+                flgActive: 1
+            })
+            .query('innerJoin', 'category_news', 'news_id', 'news.id')
+            .query('innerJoin', 'category', 'category.id', 'category_id')
+            .where('category.category', req.body.category)
+            .fetchAll({
+                withRelated: ['tags', 'images', 'categories', 'videos']
+            })
+            .then(news => {
+                res.json(news);
+            })
+            .catch(err => {
+                res.status(400).send({
+                    err: err,
+                    position: 0,
+                    message: 'Error listing news by category.'
+                });
+            });
+    } catch (err) {
+        res.status(400).send({
+            err: err,
+            position: 1,
+            message: 'Error listing news by category.'
+        });
+    }
+});
+
+router.post('/byTagByCategory', (req, res) => {
+    try {
+        News.where({
+                approval: 1,
+                flgActive: 1
+            })
+            .query('innerJoin', 'category_news', 'category_news.news_id', 'news.id')
+            .query('innerJoin', 'category', 'category.id', 'category_id')
+            .query('innerJoin', 'news_tag', 'news_tag.news_id', 'news.id')
+            .query('innerJoin', 'tag', 'tag.id', 'tag_id')
+            .where('category.category', req.body.category)
+            .where('tag.tag', req.body.tag)
+            .fetchAll({
+                withRelated: ['tags', 'images', 'categories', 'videos']
+            })
+            .then(news => {
+                res.json(news);
+            })
+            .catch(err => {
+                res.status(400).send({
+                    err: err,
+                    position: 0,
+                    message: 'Error listing news by category by tag.'
+                });
+            });
+    } catch (err) {
+        res.status(400).send({
+            err: err,
+            position: 1,
+            message: 'Error listing news by category by tag.'
+        });
+    }
+});
+
+router.get('/:id', (req, res) => {
+    try {
+        News.where({
+                id: req.params.id,
+                approval: 1,
+                flgActive: 1
+            })
+            .fetchAll({
+                withRelated: ['tags', 'images', 'categories', 'videos']
+            })
+            .then(news => {
+                res.json(news);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).send({
+                    err: err,
+                    position: 0,
+                    message: 'Error listing news by id.'
+                });
+            });
+    } catch (err) {
+        console.log(err);
+        res.status(400).send({
+            err: err,
+            position: 1,
+            message: 'Error listing news by id.'
         });
     }
 });
---
+++ src/app/controllers/videoController.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const router = require('express').Router();
+const Video = require('./../models/video');
+
+router.post('/byTag', (req, res) => {
+    try {
+        Video
+            .query('innerJoin', 'tag_video', 'video_id', 'video.id')
+            .query('innerJoin', 'tag', 'tag.id', 'tag_id')
+            .where('tag.tag', req.body.tag)
+            .fetchAll({
+                withRelated: ['tags']
+            })  
+            .then(videos => {
+                res.json(videos);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).send({
+                    err: err,
+                    position: 0,
+                    message: 'Error listing videos by tag.'
+                });
+            });
+    } catch (err) {
+        res.status(400).send({
+            err: err,
+            position: 1,
+            message: 'Error listing videos by tag.'
+        });
+    }
+});
+
+
+module.exports = app => app.use('/video', router);
